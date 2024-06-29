b2fa0bb9a566f561336a2333306f8241754d5489
a8901147 <34620271+a8901147@users.noreply.github.com>
2024-06-29 16:46:25 +0800

    Time: 0 ms (100%), Space: 5.8 MB (29.17%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.c
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapPairs(struct ListNode* head) {
+    int counter = 2;
+    struct ListNode* prev_last = NULL;
+    struct ListNode* first = NULL;
+    struct ListNode* cur = head;
+
+    struct ListNode* prev = NULL;
+    struct ListNode* nxt = NULL;
+    while(cur){
+        // get first node
+        first = cur;
+
+        // loop k times
+        while (counter && cur){
+            cur = cur->next;
+            counter -= 1;
+        }
+
+        if (counter){
+            break;
+        }
+        else{
+            //reverse list
+            cur = first;
+            for (int i = 0; i<2; i++){
+                nxt = cur->next;
+                cur->next=prev;
+
+                prev = cur;
+                cur = nxt;
+            }
+
+            // assign new head
+            if(!prev_last){
+                head = prev;
+            }
+
+            // connect two list
+            if (prev_last){
+                prev_last->next = prev;
+            }
+            
+            first->next = cur;
+            prev_last = first;
+            counter = 2;
+        }
+    }
+    return head;
+}
